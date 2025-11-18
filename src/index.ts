9cf279874bd6690ab56567a93a90c1d16a38e457
Your Name <you@example.com>
2025-11-18 09:05:05 +0100

    Poprawa

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,52 @@
-type FiguraGeometryczna = {
-    typ: string,
-
-    bok: number | undefined,
-    promien: number | undefined,
-    dlugosc: number | undefined,
-    szerokosc: number | undefined,
-    wysokosc: number | undefined,
-    podstawa: number | undefined,
+type DaneKwadrat = {
+    typ: 'kwadrat',
+    bok: number,
+}
+type DaneKolo = {
+    typ: 'kolo',
+    promien: number,
+}
+type DaneProstokat = {
+    typ: 'prostokat',
+    dlugosc: number,
+    szerokosc: number,
+}
+type DaneTrojkat = {
+    typ: 'trojkat',
+    wysokosc: number,
+    podstawa: number,
 }
 
-function obliczPole(figura: FiguraGeometryczna):number {
-    if(figura.typ === 'kwadrat') {
-        if(typeof figura.bok === 'number') {
-            return Math.pow(figura.bok, 2);
-        }
-        else {
-            throw new Error(`${figura.bok} jest nie zdefiniowana`);
-        }
-    }
-    else if(figura.typ === 'kolo') {
-        if(typeof figura.promien === 'number') {
-            return Math.PI * Math.pow(figura.promien, 2);
-        }
-        else {
-            throw new Error(`${figura.promien} jest nie zdefiniowana`);
-        }
-    }
-    else if(figura.typ === 'prostokat') {
-        if(typeof figura.dlugosc === 'number' && typeof figura.szerokosc === 'number') {
-            return figura.dlugosc * figura.szerokosc;
-        }
-        else {
-            throw new Error(`${figura.dlugosc} lub ${figura.szerokosc} jest nie zdefiniowana`);
-        }
-    }
-    else if(figura.typ === 'trojkat') {
-        if(typeof figura.wysokosc === 'number' && typeof figura.podstawa === 'number') {
-            return (figura.podstawa * figura.wysokosc)/2;
-        }
-        else {
-            throw new Error(`${figura.wysokosc} lub ${figura.podstawa} jest nie zdefiniowana`);
-        }
-    }
-    else {
-        throw new Error("Błędna nazwa figury");
+type FiguraGeometryczna = DaneKwadrat | DaneKolo | DaneProstokat | DaneTrojkat;
+
+function obliczPole(figura: FiguraGeometryczna): number {
+    if (figura.typ === 'kwadrat') {
+        return figura.bok * figura.bok;
+    } else if (figura.typ === 'kolo') {
+        return Math.PI * Math.pow(figura.promien, 2);
+    } else if (figura.typ === 'prostokat') {
+        return figura.dlugosc * figura.szerokosc;
+    } else if (figura.typ === 'trojkat') {
+        return (figura.podstawa * figura.wysokosc) / 2;
+    } else {
+        throw new Error("Nieznany typ figury");
     }
 }
+
 
 let figura1:FiguraGeometryczna = {
-    typ: "kwadrat",
-    bok: 12,
-    promien: undefined,
-    dlugosc: undefined,
-    szerokosc: undefined,
-    wysokosc: undefined,
-    podstawa: undefined,
-}
-let figura2:FiguraGeometryczna = {
-    typ: "trapez",
-    bok: 1234,
-    promien: 64363,
-    dlugosc: 6565,
-    szerokosc: 53452,
-    wysokosc: 2333,
-    podstawa: 11,
+    typ: 'kwadrat',
+    bok: 34,
 }
 let figura3:FiguraGeometryczna = {
     typ: "trojkat",
-    bok: undefined,
-    promien: undefined,
-    dlugosc: undefined,
-    szerokosc: undefined,
     wysokosc: 12,
-    podstawa: undefined,
+    podstawa: 6,
 }
 
 console.log(obliczPole(figura1));
 console.log(obliczPole({
-    typ: "kolo",
-    bok: undefined,
-    promien: 5,
-    dlugosc: undefined,
-    szerokosc: undefined,
-    wysokosc: undefined,
-    podstawa: undefined,
-}));
-console.log(obliczPole(figura2));//daje błąd
-console.log(obliczPole(figura3))//daje błąd
+        typ: "kolo",
+        promien: 67,
+    }));
+console.log(obliczPole(figura3))
